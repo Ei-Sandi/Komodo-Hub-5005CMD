5529ae8ed68a6d7c064c354b3d16f4654a14e832
Ei Sandi Aung <150097294+Ei-Sandi@users.noreply.github.com>
2025-02-07 17:01:37 +0000

    Added initial public files

---
+++ KOMODO HUB/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+#main page logo bar buttons
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
+@app.route("/register/")
+def register():
+    return render_template("register.html")
+
+#nav bar buttons
+@app.route("/news/")
+def news():
+    return render_template("news.html")
+
+@app.route("/forums/")
+def forums():
+    return render_template("forums.html")
+
+@app.route("/about-us/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
