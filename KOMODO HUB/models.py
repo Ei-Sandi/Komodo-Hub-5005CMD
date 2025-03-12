0185fb6d0efdff25cba737b44d4d5817edd3c611
Ei-Sandi <150097294+Ei-Sandi@users.noreply.github.com>
2025-03-12 18:47:08 +0000

    clean codes, UI, added login system

---
+++ KOMODO HUB/models.py
@@ -0,0 +1,35 @@
+from flask_login import UserMixin
+from app import db
+
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+
+    uid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(120), unique=True, nullable = False)
+    first_name = db.Column(db.String(50), nullable = False)
+    last_name = db.Column(db.String(50), nullable = False)
+    dob = db.Column(db.String(10), nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+    code = db.Column(db.String(10))
+    role = db.Column(db.String(10), default = "student")
+
+    def __repr__(self):
+        return f"<User: {self.username}, Role: {self.role}>"
+    
+    def get_id(self):
+        return self.uid
+    
+class Organisation(db.Model):
+    __tablename__ = "organisation"
+    org_name = db.Column(db.String(200), primary_key=True)
+    province = db.Column(db.String(120))
+    country = db.Column(db.String(60))
+    logo_img = db.Column(db.Text)
+    logo_name = db.Column(db.Text)
+    logo_type = db.Column(db.Text)
+    intro = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"Reg_Org('{self.org_name})"
+
---
+++ KOMODO HUB/routes.py
@@ -0,0 +1,132 @@
+from flask import render_template, request, redirect, url_for, session
+from flask_login import login_user, logout_user, current_user, login_required
+from models import User,Organisation
+from werkzeug.utils import secure_filename
+
+def all_routes(app):
+    @app.route("/")
+    @app.route("/home/")
+    def home():
+        if current_user.is_authenticated:
+            username = current_user.username
+            role = current_user.role
+            return redirect(url_for("dashboard", username=username, role=role))
+        else:
+            return render_template("home.html")
+    
+    @app.route("/forums/")
+    def forums():
+        return render_template("forums.html")
+
+    @app.route("/contact/")
+    def contact():
+        return render_template("contact.html")
+
+    @app.route("/volunteer/")
+    def volunteer():
+        return render_template("volunteer.html")
+
+    @app.route("/discussion/")
+    def discussion():
+        return render_template("discussion.html")
+
+    #testing privatemain template
+    @app.route("/privatemain/")
+    def privatemain():
+        return render_template("PrivateMain.html")
+    
+def register_routes(app, db, bcrypt):
+    @app.route("/register/")
+    def register():
+        return render_template("register.html")
+    
+    @app.route("/register/individual/", methods=['POST', 'GET'])
+    def individual():    
+        if request.method == 'GET':
+            return render_template('RegInd.html')
+        elif request.method == 'POST':
+            username = request.form['User']
+            email = request.form['Email']
+            firstName = request.form['First']
+            lastName = request.form['Last']
+            dob = request.form['Date']
+            password = request.form['Pass']
+            hashed_password = bcrypt.generate_password_hash(password)
+            code = request.form['Code']
+
+            #check if username already exists
+            userCheck = User.query.filter(User.username == username).first()
+            if userCheck:
+                return redirect(url_for("individual"))
+            
+            #check if email already exists
+            emailCheck = User.query.filter(User.email == email).first()
+            if emailCheck:
+                return redirect(url_for("individual"))
+
+            #add new user into database
+            new_user = User(username = username,email = email, first_name = firstName, last_name = lastName, dob = dob, password = hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('login'))
+        
+    @app.route('/register/organisation/', methods = ['POST', 'GET'])
+    def organisation():
+        if request.method == 'GET':
+            return render_template('RegOrg.html')
+        if request.method == 'POST':
+            orgName = request.form['orgName']
+            province = request.form['province']
+            country = request.form['country']
+            intro = request.form['intro']
+            logo = request.files['filename']
+            filename = secure_filename(logo.filename)
+            mimetype = logo.mimetype
+
+            newOrg = Organisation(org_name = orgName, province = province, country = country, intro = intro, logo_img = logo.read() , logo_name = filename, logo_type = mimetype)
+
+            db.session.add(newOrg)
+            db.session.commit()
+            return redirect("/register/individual/")
+        
+def login_routes(app, bcrypt):        
+    @app.route("/login/", methods=["GET", "POST"])
+    def login():  
+        if request.method == 'GET':
+            return render_template('login.html')
+        elif request.method == 'POST':
+            username = request.form.get("username")
+            password = request.form.get("password")
+
+            user = User.query.filter(User.username == username).first()
+
+            if bcrypt.check_password_hash(user.password,password):
+                login_user(user)
+                session["username"] = username
+                return redirect(url_for("home"))
+            else:
+                return redirect(url_for("login"))
+
+    @app.route("/login/forgetpass")
+    def forget():
+        return render_template("forget_password.html")
+
+    @app.route("/login/forgetpass/resetpass")
+    def resetpass():
+        return render_template("reset_password.html")
+    
+    @app.route("/logout")
+    def logout():
+        logout_user()
+        return redirect(url_for("home"))
+    
+def restricted_routes(app):
+    @app.route("/dashboard")
+    @login_required
+    def dashboard():
+        if "username" in session:
+            return render_template("dashboard.html", username = session['username'])
+    
+
+
+
