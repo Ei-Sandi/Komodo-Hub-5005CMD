2f68fbf7291ce36101cf64471d48146fe9a04539
ReetikaRT <reetika.thapar03@gmail.com>
2025-03-13 12:05:51 +0000

    Inhert from PrivateMain

--- KOMODO HUB/run.py
+++ KOMODO HUB/run.py
@@ -5,4 +5,4 @@ flask_app = create_app()
 socketio = SocketIO(flask_app)
 
 if __name__ == "__main__":
-    socketio.run(flask_app, debug=True)
+    socketio.run(flask_app, debug=True, host="localhost")
