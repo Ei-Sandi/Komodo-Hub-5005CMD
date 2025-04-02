1f1b7994f310e45fb1de3c64054abccf9dac1579
faizat0506 <faizatfadekemi@gmail.com>
2025-04-02 15:59:41 +0100

    fix conflits

--- KOMODO HUB/routes.py
+++ KOMODO HUB/routes.py
@@ -216,6 +216,8 @@ def restricted_routes(app):
     @app.route("/dashboard/")
     @login_required
     def dashboard():
+        if "username" in session:
+            return render_template("dashboard.html", username = session['username'])
         if current_user.role == 'principal':
             return redirect(url_for("principal_dashboard"))
         else:
