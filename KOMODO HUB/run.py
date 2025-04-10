307a49fb852076ed2b18d0a19a4169cb7659ef94
Ashmitha-13 <ashmithasfc@gmail.com>
2025-04-10 00:07:19 +0000

    All sorted

--- KOMODO HUB/run.py
+++ KOMODO HUB/run.py
@@ -5,8 +5,4 @@ flask_app = create_app()
 socketio = SocketIO(flask_app, cors_allowed_origins="*")
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     socketio.run(flask_app, debug=True, host="localhost")
-=======
-    socketio.run(flask_app, debug=True, host="localhost")
->>>>>>> f56c54d104846e56db8efb80dc9c69408f61594e
